ba4a929fc5e466a5cbf560cb2ec7e69595160194
Asmir Kopić <67395100+AsmirKopic@users.noreply.github.com>
2021-10-19 23:47:21 +0200

    git comm 1

---
+++ src/com/tutorial/springdemo/TrackCoach.java
@@ -0,0 +1,17 @@
+package com.tutorial.springdemo;
+
+public class TrackCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		return "run a hard 5k";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+
+}
