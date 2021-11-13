b4b9f35e895daee0197de4cc01f783f06e5fae73
Asmir Kopić <67395100+AsmirKopic@users.noreply.github.com>
2021-11-13 18:03:58 +0100

    hibernate one to one relation

---
+++ hb-01-one-to-one-uni/src/com/tutorial/hibernate/demo/CreateStudentDemo.java
@@ -0,0 +1,39 @@
+package com.tutorial.hibernate.demo;
+
+import org.hibernate.cfg.Configuration;
+import org.hibernate.SessionFactory;
+import org.hibernate.Session;
+
+import com.tutorial.hibernate.demo.entity.Student;
+
+public class CreateStudentDemo {
+
+	public static void main(String[] args) {
+		
+		SessionFactory factory = new Configuration()
+									.configure("hibernate.cfg.xml")
+									.addAnnotatedClass(Student.class)
+									.buildSessionFactory();
+		
+		Session session = factory.getCurrentSession();
+		
+		try {
+			// use session object to save java object
+			Student theStudent = new Student("Aida", "Kopic", "asmir@email.com");
+			
+			System.out.println("Beggining transaction...");
+			session.beginTransaction();
+			
+			System.out.println("Saving the object..");
+			session.save(theStudent);
+			
+			session.getTransaction().commit();
+			System.out.println("Done.");
+			
+		} finally {
+			session.close();
+		}
+	
+	}
+
+}
