af195815a27771d4686208faed46cf9d791d5a07
Asmir Kopić <67395100+AsmirKopic@users.noreply.github.com>
2021-11-30 05:52:41 +0100

    spring aop setup files

---
+++ src/com/tutorial/aopdemo/dao/AccountDAO.java
@@ -0,0 +1,14 @@
+package com.tutorial.aopdemo.dao;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class AccountDAO {
+	
+	public void addAccount() {
+		System.out.println(getClass() + "Doing db work > Adding account");
+	}
+	
+	
+
+}
