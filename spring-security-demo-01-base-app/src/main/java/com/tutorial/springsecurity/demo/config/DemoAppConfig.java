c9b858ce3ef23488e559e81eca0b62e62b4542eb
Asmir Kopić <67395100+AsmirKopic@users.noreply.github.com>
2021-12-12 22:33:09 +0100

    added bcrypt password into database

--- spring-security-demo-01-base-app/src/main/java/com/tutorial/springsecurity/demo/config/DemoAppConfig.java
+++ spring-security-demo-01-base-app/src/main/java/com/tutorial/springsecurity/demo/config/DemoAppConfig.java
@@ -1,16 +1,33 @@
 package com.tutorial.springsecurity.demo.config;
+import java.beans.PropertyVetoException;
+import java.util.logging.Logger;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.tutorial.springsecurity.demo")
+@PropertySource("classpath:persistence-mysql.properties")
 public class DemoAppConfig {
+	
+	// set up variable to hold the properties (@PropertySource("classpath:persistence-mysql.properties"))
+	@Autowired
+	private Environment env;
+	
+	// set up logger for diagnostic
+	private Logger logger = Logger.getLogger(getClass().getName());
 	
 	//define bean for view resolver
 	@Bean
@@ -23,5 +40,58 @@ public class DemoAppConfig {
 		
 		return viewResolver;
 	}
+	
+	// define a bean for our security datasource
+	
+	@Bean
+	public DataSource securityDataSource() {
+		
+		// create connection pool
+		ComboPooledDataSource securityDataSource = new ComboPooledDataSource();
+		
+		// set jdbc driver class
+		try {
+			securityDataSource.setDriverClass(env.getProperty("jdbc.driver"));
+		} catch (PropertyVetoException exc) {
+			throw new RuntimeException(exc);
+		}
+		
+		// log the connection props
+		logger.info(">>> jdbc.url = " + env.getProperty("jdbc.url"));
+		logger.info(">>> jdbc.user = " + env.getProperty("jdbc.user"));
+		
+		// set database connection properties
+		securityDataSource.setJdbcUrl(env.getProperty("jdbc.url"));
+		securityDataSource.setUser(env.getProperty("jdbc.user"));
+		securityDataSource.setPassword(env.getProperty("jdbc.password"));
+		
+		// set connection pool props
+		securityDataSource.setInitialPoolSize(getIntProperty("connection.pool.initialPoolSize"));
+		securityDataSource.setMinPoolSize(getIntProperty("connection.pool.minPoolSize"));
+		securityDataSource.setMaxPoolSize(getIntProperty("connection.pool.maxPoolSize"));
+		securityDataSource.setMaxIdleTime(getIntProperty("connection.pool.maxIdleTime"));
+		
+		
+		return securityDataSource;
+	}
+	
+	/*
+	 * Helper method to read environment property and convert to int
+	 */
+	private int getIntProperty(String propName) {
+		
+		String propValue = env.getProperty(propName);
+		
+		int intPropValue = Integer.parseInt(propValue);
+		
+		return intPropValue;
+	}
+	
+	
+	
+	
+	
+	
+	
 
 }
--- spring-security-demo-01-base-app/src/main/java/com/tutorial/springsecurity/demo/config/DemoSecurityConfig.java
+++ spring-security-demo-01-base-app/src/main/java/com/tutorial/springsecurity/demo/config/DemoSecurityConfig.java
@@ -1,5 +1,8 @@
 package com.tutorial.springsecurity.demo.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,16 +15,22 @@ import org.springframework.security.core.userdetails.User.UserBuilder;
 @EnableWebSecurity
 public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	private DataSource securityDataSource;
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		
 		//add our users for in memory authentication
 		
-		UserBuilder users = User.withDefaultPasswordEncoder();
-		auth.inMemoryAuthentication()
-			.withUser(users.username("john").password("test123").roles("EMPLOYEE"))
-			.withUser(users.username("mery").password("test123").roles("EMPLOYEE", "MANAGER"))
-			.withUser(users.username("asmir").password("test123").roles("EMPLOYEE","ADMIN"));
+//		UserBuilder users = User.withDefaultPasswordEncoder();
+//		auth.inMemoryAuthentication()
+//			.withUser(users.username("john").password("test123").roles("EMPLOYEE"))
+//			.withUser(users.username("mery").password("test123").roles("EMPLOYEE", "MANAGER"))
+//			.withUser(users.username("asmir").password("test123").roles("EMPLOYEE","ADMIN"));
+		
+		auth.jdbcAuthentication().dataSource(securityDataSource);
+		
 	}
 
 	@Override
