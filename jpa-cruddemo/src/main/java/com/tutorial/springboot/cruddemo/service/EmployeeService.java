19de01a093f39b5c41abffce561802942ed3f82b
Asmir Kopić <67395100+AsmirKopic@users.noreply.github.com>
2021-12-26 00:10:11 +0100

    crud app demo using jpa

---
+++ jpa-cruddemo/src/main/java/com/tutorial/springboot/cruddemo/service/EmployeeService.java
@@ -0,0 +1,16 @@
+package com.tutorial.springboot.cruddemo.service;
+
+import com.tutorial.springboot.cruddemo.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    public List<Employee> findAll();
+
+    public Employee findById(int theId);
+
+    public void save(Employee theEmployee);
+
+    public void deleteById(int theId);
+}
